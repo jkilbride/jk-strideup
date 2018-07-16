9f4cbb5bdde82cd7111efe2e3b2f2e8bc0d7f83f
John Kilbride <jkilbride@gmail.com>
2018-07-16 15:51:52 +0100

    Database

--- apply.php
+++ apply.php
@@ -1,8 +1,13 @@
 <?php
     echo 'Apply';
-    $db = parse_url(getenv("DATABASE_URL"));
-    $db["path"] = ltrim($db["path"], "/");
+    $dbini = parse_url(getenv("DATABASE_URL"));
+    $dbini["path"] = ltrim($dbini["path"], "/");
+    print_r($dbini);
+
+    $db = new PDO($dbini);
+
     print_r($db);
+
 
 ?>
 <img src="images/apply_Full.png" width="100%"/>
