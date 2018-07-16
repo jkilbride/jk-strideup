1e32a147e0d86bd6688533b492348d6d983cb1a9
John Kilbride <jkilbride@gmail.com>
2018-07-16 15:46:25 +0100

    Database

--- apply.php
+++ apply.php
@@ -1,4 +1,8 @@
 <?php
     echo 'Apply';
+    $db = parse_url(getenv("DATABASE_URL"));
+    $db["path"] = ltrim($db["path"], "/");
+    print_r($db);
+
 ?>
 <img src="images/apply_Full.png" width="100%"/>
